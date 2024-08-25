308b2645e8b348715d14859947728f0d0dfa7f4d
Yoel Moya Carmona <yoel.moya.carmona@gmail.com>
2024-08-25 15:56:02 -0600

    Ejercicio 3
    Este es el codigo funcional del ejercicio 3 de la segunda práctica de la asignación 1.

--- Laboratorios/L2/ScrollLEDs/src/ScrollLEDs.c
+++ Laboratorios/L2/ScrollLEDs/src/ScrollLEDs.c
@@ -5,10 +5,52 @@
 #define READ_GPIO(dir) (*(volatile unsigned *)dir)
 #define WRITE_GPIO(dir, value) { (*(volatile unsigned *)dir) = (value); }
 
+void delay(int timer);
+
 int main ( void ){
 
+    int En_Value=0xFFFF;
+    int timer = 5000000;
+    int LEDs_value = 0x0001;
 
+    WRITE_GPIO(GPIO_INOUT, En_Value)
+
+    while (1){
+        
+        if (LEDs_value == 0xFFFF)
+        {
+            LEDs_value = 0x0001;
+            WRITE_GPIO(GPIO_LEDs, LEDs_value);
+            delay(timer);
+        }
+
+        while ((LEDs_value & 0x8000) == 0)
+        {
+            LEDs_value = LEDs_value << 1;
+            WRITE_GPIO(GPIO_LEDs, LEDs_value);
+            delay(timer);
+        }
+
+        while ((LEDs_value & 0x0001) == 0)
+        {
+            LEDs_value = LEDs_value >> 1;
+            WRITE_GPIO(GPIO_LEDs, LEDs_value);
+            delay(timer);
+        }
+
+        LEDs_value = LEDs_value << 1;
+        LEDs_value++;
+        WRITE_GPIO(GPIO_LEDs, LEDs_value);
+        delay(timer);
+
+    }
          
     return(0);
 }
 
+void delay(int timer){
+    static int j, i;
+    for (i = 0; i < timer; i++){
+        j++;
+    } 
+}
