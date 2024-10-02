67211e3f4ac2364d59529567492f71c3e279b4e4
SixtoLoria <sixtoloria@gmail.com>
2024-10-02 08:41:31 -0600

    Correccion del codigo.

--- Laboratorios/Asignacion 2/Cuenta_ascendente_bin/src/Cuenta_asendente_bin.c
+++ Laboratorios/Asignacion 2/Cuenta_ascendente_bin/src/Cuenta_asendente_bin.c
@@ -1,6 +1,8 @@
 // memory-mapped I/O addresses
-#define GPIO_LEDs 0x80001404
+#define GPIO_LEDs  0x80001404
 #define GPIO_Boton 0x80001800
+#define GPIO_INOUT 0x80001408
+
 #define READ_GPIO(dir) (*(volatile unsigned *)dir)
 #define WRITE_LEDS(dir, value) {(*(volatile unsigned *)dir) = (value);}
 
@@ -9,30 +11,43 @@
 int main (void)
 {
 
+
 // Valores iniciales
-unsigned int DELAY = 2000000;
+int enable_value = 0xFFFF;
+unsigned int DELAY = 3000000;
 unsigned int boton_value = 0;
-int posicion_Led = 0x00008000;  // Posicion inicial del led (solo uno encendido).
+int posicion_Led = 0x00000001;  // Posicion inicial del led (solo uno encendido).
+
+WRITE_LEDS(GPIO_INOUT, enable_value);
+
+static int i,j,k;
 
 while(1){
     // Desplazamieno de los leds a la izquierda
-    for(int i = 0; i < (16); i++)
+    for( i = 0; i < (16); i++)
     {
         boton_value = READ_GPIO(GPIO_Boton);
         WRITE_LEDS(GPIO_LEDs, posicion_Led);       
-        posicion_Led = posicion_Led >> 1;            // Se desplazando a la derecha
+        posicion_Led = posicion_Led + 1;            // Se desplazando a la derecha
 
         // Si el valor de los botones es igual a BTNC cambiamos la velocidad
         if (boton_value & 0x1){
-        DELAY = DELAY - 5000;
+            if (DELAY <= 50){
+                DELAY = 3000000;
+            }
+            DELAY = DELAY - 500000;
         }
-        // Si se lee BTNU reiniciamos el timer
+
+        // Si se lee BTNU reiniciamos el timer y el valor inicial de leds
         else if (boton_value & 0x2){
-        DELAY = 2000000;
+        DELAY = 3000000;
+        posicion_Led = 0x00000001;  // retornando posicion inicial
         }
-        for (int k = 0; k < DELAY; k++){}
+
+        //Implementacion del retardo
+        for (k = 0; k < DELAY; k++){
+            j++;
+        }
     }
-    posicion_Led = 0x00008000;  // retornando posicion inicial
 }
-return (0);
 }
